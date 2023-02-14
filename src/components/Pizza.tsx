1ba64e52cf2a506289e44ba5f86521a55d68ccb3
Ganiev Davlet <Ganiev.Davlet@gmail.com>
2023-02-14 23:21:59 +0300

    feat: 46. Creating a custom hook

--- src/components/Pizza.tsx
+++ src/components/Pizza.tsx
@@ -3,16 +3,19 @@ import React from "react";
 // types
 import { IPizza } from "../types";
 
-import { AddToCartProps, withAddToCart } from "./AddToCart";
+// hooks
+import { useAddToCart } from "./AddToCart";
 
 // styles
 import styles from "./Pizza.module.css";
 
-interface IProps extends AddToCartProps {
+interface IProps {
   pizza: IPizza;
 }
 
-const PizzaItem: React.FC<IProps> = ({ pizza, addToCart }) => {
+const PizzaItem: React.FC<IProps> = ({ pizza }) => {
+  const addToCart = useAddToCart();
+
   const handleAddToCartClick = () => {
     addToCart({
       id: pizza.id,
@@ -26,11 +29,21 @@ const PizzaItem: React.FC<IProps> = ({ pizza, addToCart }) => {
       <h2>{pizza.name}</h2>
       <p>{pizza.description}</p>
       <p>{pizza.price}</p>
-      <button type="button" onClick={handleAddToCartClick}>
+
+      <button
+        type="button"
+        onClick={() =>
+          addToCart({
+            id: pizza.id,
+            name: pizza.name,
+            price: pizza.price,
+          })
+        }
+      >
         Добавить пиццу
       </button>
     </li>
   );
 };
 
-export default withAddToCart(PizzaItem);
+export default PizzaItem;
